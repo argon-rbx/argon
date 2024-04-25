976843f35e8a07835da785e4decbc88ba463fc8c
Dervex <dervexdev@gmail.com>
2024-04-25 19:45:27 +0200

    Improve `plugin` command

--- src/cli/plugin.rs
+++ src/cli/plugin.rs
@@ -1,12 +1,15 @@
 use anyhow::Result;
-use clap::Parser;
-use std::path::PathBuf;
+use clap::{Parser, ValueEnum};
+use std::{fs, path::PathBuf};
 
 use crate::{argon_info, installer, util};
 
 /// Install Argon Roblox Studio plugin locally
 #[derive(Parser)]
 pub struct Plugin {
+	/// Whether to `install` or `uninstall` the plugin
+	#[arg(hide_possible_values = true)]
+	mode: Mode,
 	/// Custom plugin installation path
 	#[arg()]
 	path: Option<PathBuf>,
@@ -15,15 +18,34 @@ pub struct Plugin {
 impl Plugin {
 	pub fn main(self) -> Result<()> {
 		let plugin_path = if let Some(path) = self.path {
-			path
+			if path.is_dir() {
+				path.join("Argon.rbxm")
+			} else {
+				path
+			}
 		} else {
 			util::get_plugin_path()?
 		};
 
-		argon_info!("Installing Argon plugin..");
-
-		installer::install_plugin(&plugin_path, true)?;
+		match self.mode {
+			Mode::Install => {
+				argon_info!("Installing Argon plugin..");
+				installer::install_plugin(&plugin_path, true)?;
+			}
+			Mode::Uninstall => {
+				argon_info!("Uninstalling Argon plugin..");
+				fs::remove_file(plugin_path)?;
+			}
+		}
 
 		Ok(())
 	}
+}
+
+#[derive(ValueEnum, Clone)]
+enum Mode {
+	/// Install the plugin in the selected path or in Studio's plugin directory
+	Install,
+	/// Uninstall the plugin from the selected path or from Studio's plugin directory
+	Uninstall,
 }
